18f6142be7b8bd370546ccd96da2273d4ad8694d
alladon <d.ipatov.v@gmail.com>
2022-12-31 15:42:03 +0200

    fix ico

--- src/classes/ServerRouter.ts
+++ src/classes/ServerRouter.ts
@@ -19,14 +19,15 @@ export default class ServerRouter {
         this.router.get("/api/maps", this.controller.api.maps.bind(this.controller));
         this.router.get("/api/analyze", this.controller.api.analyze.bind(this.controller));
         this.router.get("/api/map/:name", this.controller.api.map.bind(this.controller));
-        this.router.get("/:name", (req, res) => {
-            if (
-                req.params.name !== "scripts" &&
-                req.params.name !== "pages" &&
-                req.params.name !== "images" &&
-                req.params.name !== "styles"
-            )
-                res.redirect("/");
-        });
+        // this.router.get("/:name", (req, res) => {
+        //     if (
+        //         req.params.name !== "scripts" &&
+        //         req.params.name !== "pages" &&
+        //         req.params.name !== "images" &&
+        //         req.params.name !== "styles" &&
+        //         req.params.name !== "favicon.ico"
+        //     )
+        //         res.redirect("/");
+        // });
     }
 }
